71daba7c6fbb4d25f263deb6a405d96074a4c000
adam <financeCoding@gmail.com>
2013-01-11 17:29:26 -0800

    Adjusting example code to run on Dart VM version: 0.1.2.0_r17004 (Fri Jan 11 16:52:26 2013)

--- example/DeltaBlue.dart
+++ example/DeltaBlue.dart
@@ -448,7 +448,7 @@ class Variable {
 
   /// Removes all traces of c from this variable.
   void removeConstraint(Constraint c) {
-    constraints = constraints.filter((e) => c != e);
+    constraints = constraints.where((e) => c != e).toList();
     if (determinedBy == c) determinedBy = null;
   }
 }
