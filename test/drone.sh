4a83aebd97fdb23eb3dac9ff52ad0c89340eba33
devoncarew@google.com <devoncarew@google.com>
2014-03-12 13:03:42 -0700

    add an all.dart test suite; fix an analysis issue

--- test/drone.sh
+++ test/drone.sh
@@ -1,12 +1,11 @@
 # get the packages
 pub install
 
-# analyzer the code
+# analyze the code
 dartanalyzer example/Template.dart
 dartanalyzer lib/benchmark_harness.dart
+dartanalyzer test/all.dart
 dartanalyzer test/benchmark_harness_test.dart
-dartanalyzer test/result_emitter_test.dart
 
 # run the tests
-dart test/benchmark_harness_test.dart
-dart test/result_emitter_test.dart
+dart test/all.dart
