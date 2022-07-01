6a6598f6bfb11bf645d73e0895c0aab5f5192590
Vyacheslav Egorov <vegorov@google.com>
2022-07-01 15:01:02 +0200

    Use doc-comments

--- lib/src/benchmark_base.dart
+++ lib/src/benchmark_base.dart
@@ -8,33 +8,36 @@ class BenchmarkBase {
   final String name;
   final ScoreEmitter emitter;
 
-  // Empty constructor.
   const BenchmarkBase(this.name, {this.emitter = const PrintEmitter()});
 
-  // The benchmark code.
-  // This function is not used, if both [warmup] and [exercise] are overwritten.
+  /// The benchmark code.
+  ///
+  /// This function is not used, if both [warmup] and [exercise] are overwritten.
   void run() {}
 
-  // Runs a short version of the benchmark. By default invokes [run] once.
+  /// Runs a short version of the benchmark. By default invokes [run] once.
   void warmup() {
     run();
   }
 
-  // Exercises the benchmark. By default invokes [run] 10 times.
+  /// Exercises the benchmark. By default invokes [run] 10 times.
   void exercise() {
     for (var i = 0; i < 10; i++) {
       run();
     }
   }
 
-  // Not measured setup code executed prior to the benchmark runs.
+  /// Not measured setup code executed prior to the benchmark runs.
   void setup() {}
 
-  // Not measures teardown code executed after the benchmark runs.
+  /// Not measured teardown code executed after the benchmark runs.
   void teardown() {}
 
-  // Measures the score for this benchmark by executing it repeatedly until
-  // time minimum has been reached.
+  /// Measures the score for this benchmark by executing it enough times
+  /// to reach [minimumMillis].
+  ///
+  /// This function searches for the requisite number of iterations by
+  /// starting at [initialIter].
   static _Measurement _measureForImpl(
       void Function() f, int minimumMillis, int initialIter) {
     final minimumMicros = minimumMillis * 1000;
@@ -58,12 +61,12 @@ class BenchmarkBase {
     }
   }
 
-  // Measures the score for this benchmark by executing it repeatedly until
-  // time minimum has been reached.
+  /// Measures the score for this benchmark by executing it repeatedly until
+  /// time minimum has been reached.
   static double measureFor(void Function() f, int minimumMillis) =>
       _measureForImpl(f, minimumMillis, 1).score;
 
-  // Measures the score for the benchmark and returns it.
+  /// Measures the score for the benchmark and returns it.
   double measure() {
     setup();
     // Warmup for at least 100ms. Discard result.
